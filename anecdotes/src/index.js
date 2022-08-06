d2159032822cdb143264d24cb39cfabbdcba70d6
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-08-06 22:41:10 +0300

    More Redux-store-configurate to separate file

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
 import App from "./App";
-import reducer from "./reducers/anecdoteReducer";
-
-const store = configureStore({ reducer });
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
