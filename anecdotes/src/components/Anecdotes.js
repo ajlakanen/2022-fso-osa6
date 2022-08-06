205247b481c5424a80646897df0a69bf7de6ae6f
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-08-06 14:02:53 +0300

    Tehtävä 6.3

---
+++ anecdotes/src/components/Anecdotes.js
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import { vote } from "../reducers/anecdoteReducer";
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <li>
+      {anecdote.content}
+      <br />
+      has {anecdote.votes} votes <button onClick={handleClick}>vote</button>
+    </li>
+  );
+};
+
+const Anecdotes = () => {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+
+  return (
+    <ul>
+      {anecdotes.map((anecdote) => (
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => dispatch(vote(anecdote.id))}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Anecdotes;
